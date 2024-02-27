2eeca029705af0498291756466dd95188fa1035c
Marco Berube <mberube@gmail.com>
2024-02-27 10:21:08 -0500

    Update server.js
    updated to provide flag

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('FLAG_HELLO_99');
 });
 server.listen(8080, function() {
 console.log('Server running on port 8080');
