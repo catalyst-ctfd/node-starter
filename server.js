e5b2ff9380629badc53d5e93f5ef92be73a4ed17
nmalvankar <nmalvankar@gmail.com>
2024-02-28 14:58:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   // Extract the domain name from the request headers
   const host = req.headers.host;
-
+  const hello = 'SEVMTE9fV09STEQ=';
+  const aloha = 'QUxPSEFfR1JFRVRJTkc='
+  
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   if (host && host.includes('aloha')) {
-    res.end('ALOHA_GREETING');
+    res.end(Buffer.from(aloha, 'base64').toString('utf-8'));
   }
   else {
-    res.end('HELLO_WORLD');
+    res.end(Buffer.from(hello, 'base64').toString('utf-8'));
   }
 });
 server.listen(8080, function() {
