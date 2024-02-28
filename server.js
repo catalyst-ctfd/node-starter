a58c7215d2bdd631e3f16857ec48818031cf05bb
nmalvankar <nmalvankar@gmail.com>
2024-02-28 11:36:21 -0500

    Update server.js
    Aloha response

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('FLAG_HELLO_99');
+  if (req.url.includes('aloha'))
+    res.end('ALOHA');
+  else
+    res.end('FLAG_HELLO_99');
 });
 server.listen(8080, function() {
 console.log('Server running on port 8080');
